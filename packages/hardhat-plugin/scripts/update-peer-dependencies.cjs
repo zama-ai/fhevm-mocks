e9c3df66ac23f9242b56c802a9b3405d66bc0eed
0xalexbel <alexandre.belhoste@zama.ai>
2025-06-02 18:11:33 +0200

    chore: add comments to update-peer-dependencies.cjs script

--- packages/hardhat-plugin/scripts/update-peer-dependencies.cjs
+++ packages/hardhat-plugin/scripts/update-peer-dependencies.cjs
@@ -1,27 +1,26 @@
+// This script updates the version of @fhevm/mock-utils in the
+// @fhevm/hardhat-plugin/package.json `peerDependencies` entry.
+//
+// It cannot be automated by simply reading the version from
+// @fhevm/mock-utils's own package.json, because that version may not yet
+// be updated during the version bump cycle.
+
+// Usage: node update-peer-dependencies.js <new-version>
+
 const fs = require("fs");
 const path = require("path");
 
 const newVersion = process.argv[2];
 
 const pkgPath = path.resolve(__dirname, "../package.json");
-// const mockUtilsPkgPath = path.resolve(__dirname, "../../mock-utils/src/package.json");
-
-// const mockUtilsPkg = JSON.parse(fs.readFileSync(mockUtilsPkgPath, "utf8"));
 const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
-
-// if (!mockUtilsPkg.version) {
-//   console.error("❌ Could not find @fhevm/mock-utils version");
-//   process.exit(1);
-// }
 
 if (!pkg.peerDependencies) {
   pkg.peerDependencies = {};
 }
 
-//pkg.peerDependencies["@fhevm/mock-utils"] = mockUtilsPkg.version;
 pkg.peerDependencies["@fhevm/mock-utils"] = newVersion;
 
-//console.log(`Update @fhevm/mock-utils=${mockUtilsPkg.version}`);
 console.log(`Update @fhevm/mock-utils=${newVersion}`);
 
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
