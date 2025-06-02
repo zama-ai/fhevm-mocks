d4498e1b6a281f80fde504a6308d6ae9a89eabc2
0xalexbel <alexandre.belhoste@zama.ai>
2025-06-02 15:51:06 +0200

    fix
    fix

---
+++ packages/hardhat-plugin/scripts/update-peer-dependencies.cjs
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const pkgPath = path.resolve(__dirname, "../package.json");
+const mockUtilsPkgPath = path.resolve(__dirname, "../../mock-utils/src/package.json");
+
+const mockUtilsPkg = JSON.parse(fs.readFileSync(mockUtilsPkgPath, "utf8"));
+const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
+
+if (!mockUtilsPkg.version) {
+  console.error("❌ Could not find @fhevm/mock-utils version");
+  process.exit(1);
+}
+
+if (!pkg.peerDependencies) {
+  pkg.peerDependencies = {};
+}
+
+pkg.peerDependencies["@fhevm/mock-utils"] = mockUtilsPkg.version;
+
+console.log(`Update @fhevm/mock-utils=${mockUtilsPkg.version}`);
+
+fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
