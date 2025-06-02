795f5397f52d141d74e53d9f5609761ad9f8a372
0xalexbel <alexandre.belhoste@zama.ai>
2025-06-02 17:56:19 +0200

    fix: pass version as arg

--- packages/hardhat-plugin/scripts/update-peer-dependencies.cjs
+++ packages/hardhat-plugin/scripts/update-peer-dependencies.cjs
@@ -1,23 +1,27 @@
 const fs = require("fs");
 const path = require("path");
 
-const pkgPath = path.resolve(__dirname, "../package.json");
-const mockUtilsPkgPath = path.resolve(__dirname, "../../mock-utils/src/package.json");
+const newVersion = process.argv[2];
 
-const mockUtilsPkg = JSON.parse(fs.readFileSync(mockUtilsPkgPath, "utf8"));
+const pkgPath = path.resolve(__dirname, "../package.json");
+// const mockUtilsPkgPath = path.resolve(__dirname, "../../mock-utils/src/package.json");
+
+// const mockUtilsPkg = JSON.parse(fs.readFileSync(mockUtilsPkgPath, "utf8"));
 const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
 
-if (!mockUtilsPkg.version) {
-  console.error("❌ Could not find @fhevm/mock-utils version");
-  process.exit(1);
-}
+// if (!mockUtilsPkg.version) {
+//   console.error("❌ Could not find @fhevm/mock-utils version");
+//   process.exit(1);
+// }
 
 if (!pkg.peerDependencies) {
   pkg.peerDependencies = {};
 }
 
-pkg.peerDependencies["@fhevm/mock-utils"] = mockUtilsPkg.version;
+//pkg.peerDependencies["@fhevm/mock-utils"] = mockUtilsPkg.version;
+pkg.peerDependencies["@fhevm/mock-utils"] = newVersion;
 
-console.log(`Update @fhevm/mock-utils=${mockUtilsPkg.version}`);
+//console.log(`Update @fhevm/mock-utils=${mockUtilsPkg.version}`);
+console.log(`Update @fhevm/mock-utils=${newVersion}`);
 
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
