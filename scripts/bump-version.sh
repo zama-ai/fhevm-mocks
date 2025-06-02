393a0ec0376c1db6bb00595602297e566914ee73
0xalexbel <alexandre.belhoste@zama.ai>
2025-06-02 17:56:19 +0200

    fix: pass version as arg

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,14 +4,14 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 VERSION=$(npm pkg get version | tr -d '\"') 
 echo "Bump to version=${VERSION}"
 
+cd ${ROOT_DIR}/packages/hardhat-plugin/scripts
+node ./update-peer-dependencies.cjs $VERSION
+
 cd ${ROOT_DIR}/packages/mock-utils/src 
 npm version $VERSION --no-git-tag-version --allow-same-version
 
 cd ${ROOT_DIR}/packages/mock-utils/scripts
 node ./generate-version.cjs
-
-cd ${ROOT_DIR}/packages/hardhat-plugin/scripts
-node ./update-peer-dependencies.cjs
 
 cd ${ROOT_DIR}/packages/hardhat-plugin
 npm version $VERSION --no-git-tag-version --allow-same-version
